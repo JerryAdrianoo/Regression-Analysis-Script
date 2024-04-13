ec0ad871f6bf95222819aeb4bf0cc1525acc403f
Jerry Adriano <jerry.adriano@liferay.com>
2024-04-13 10:36:52 -0300

    Changes: removing unecessary columns and sufixes

--- AnalysisScript.py
+++ AnalysisScript.py
@@ -19,29 +19,30 @@ def main(file):
 
 def InsertFiles(before_file, after_file):
     try:
-        
         df_before = pd.read_csv(before_file).fillna('')
         df_after = pd.read_csv(after_file).fillna('')
         columns_before = df_before.columns.tolist()
         columns_after = df_after.columns.tolist()
         merged = pd.merge(df_before, df_after, on='Reverse Call Tree', how='outer', indicator=True, suffixes=('_Before', '_After'))
-        missing_before = merged[merged['_merge'] == 'left_only']
-        missing_after = merged[merged['_merge'] == 'right_only']
+        missing_before = merged[merged['_merge'] == 'left_only'].filter(regex='^(?!.*_After).*$')
+        missing_after = merged[merged['_merge'] == 'right_only'].filter(regex='^(?!.*_Before).*$')
         before_sorted = missing_before.sort_values(by=columns_before[0])
         after_sorted = missing_after.sort_values(by=columns_after[0])
-        
         return before_sorted, after_sorted
     except Exception as e:
         raise e
 
 def cleanData(before_file, after_file):
     try:
+        print("\nGerando arquivos CSV...")
         df_before, df_after = InsertFiles(before_file, after_file)
 
         output_before_file = "New-Before.csv"
         output_after_file = "New-After.csv"
-
-        print("Gerando arquivos CSV...")
+        
+        df_before.columns = [col.replace('_Before', '') for col in df_before.columns]
+        df_after.columns = [col.replace('_After', '') for col in df_after.columns]
+        
         df_before.to_csv(output_before_file, index=False)
         df_after.to_csv(output_after_file, index=False)
         print(f"\nResultados salvos em '{output_before_file}' e '{output_after_file}'")
