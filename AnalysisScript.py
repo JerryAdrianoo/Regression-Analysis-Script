20ce700580e41e143f62d24d4ad63c2ad246fa8c
Jerry Adriano <jerry.adriano@liferay.com>
2024-04-14 20:58:40 -0300

    Identing and separating correctly functions

--- AnalysisScript.py
+++ AnalysisScript.py
@@ -3,6 +3,7 @@ import os
 
 def main():
     os.system('clear')
+
     files = []
 
     for i in range(2):
@@ -10,13 +11,16 @@ def main():
 
         while not (os.path.exists(file) and file != "" and file.lower().endswith('.csv')):
             print(f"\nFile '{file}' not found or is not a valid CSV file.\n")
+
             file = input("\nPlease enter a valid CSV file name: ")
 
         print("\n   File was successfully inserted!   \n")
         files.append(file)
 
     generateFiles(files[0], files[1])
+
     treatData(files[0], files[1])
+
     extraLines(files[0], files[1])
 
 def treatData(before_file, after_file):
@@ -31,6 +35,7 @@ def treatData(before_file, after_file):
         columns_after = df_after.columns.tolist()
         
         merged = pd.merge(df_before, df_after, left_on=columns_before[0], right_on=columns_before[0], how='outer', indicator=True, suffixes=('_Before', '_After'))
+
         missing_before = merged[merged['_merge'] == 'left_only'].filter(regex='^(?!.*_Before).*$')
         missing_after = merged[merged['_merge'] == 'right_only'].filter(regex='^(?!.*_After).*$')
 
@@ -44,33 +49,6 @@ def treatData(before_file, after_file):
     except Exception as e:
         print(f"Error while processing files: {e}")
 
-def generateFiles(before_file, after_file):
-    try:
-        print("\n   Generating CSV files...   \n")
-        df_before, df_after = treatData(before_file, after_file)
-
-        output_before_file = "New-Before.csv"
-        output_after_file = "New-After.csv"
-
-        i = 0
-        while os.path.exists(output_before_file):
-            i += 1
-            output_before_file = f"New-Before ({i}).csv"
-
-        df_before.to_csv(output_before_file, index=False)
-
-        i = 0
-        while os.path.exists(output_after_file):
-            i += 1
-            output_after_file = f"New-After ({i}).csv"
-
-        df_after.to_csv(output_after_file, index=False)
-
-        print(f"\nResults saved in '{output_before_file}' and '{output_after_file}'")
-
-    except Exception as e:
-        print(f"Error while generating files: {e}")
-        
 def extraLines(before_file, after_file):
     try:
         df_before = pd.read_csv(before_file).fillna('')
@@ -86,6 +64,7 @@ def extraLines(before_file, after_file):
             merged = pd.merge(df_before_new, df_after_new, how='right', indicator=True)
 
             lines_merged = merged[merged['_merge'] == 'right_only'].drop('_merge', axis=1)
+
             b_sorted = df_before_new.sort_values(by=df_before_new.columns[0])
             a_sorted = lines_merged.sort_values(by=lines_merged.columns[0])
 
@@ -94,26 +73,63 @@ def extraLines(before_file, after_file):
             merged = pd.merge(df_before, df_after, how='right', indicator=True)
 
             lines_merged = merged[merged['_merge'] == 'right_only'].drop('_merge', axis=1)
+
             b_sorted = df_before.sort_values(by=df_before.columns[0])
             a_sorted = lines_merged.sort_values(by=lines_merged.columns[0])
 
             lines_after = a_sorted[~a_sorted.isin(b_sorted)].dropna()
 
+        return lines_after
+
+    except Exception as e:
+        print(f"Error while analyzing and saving extra lines: {e}")
+
+
+def generateFiles(before_file, after_file):
+    try:
+        print("\n   Generating CSV files...   \n")
+        df_extraLines = extraLines(before_file, after_file)
+        df_before, df_after = treatData(before_file, after_file)
+
+        output_before_file = "New-Before.csv"
+        output_after_file = "New-After.csv"
         output_extra_file = "Extra-Lines-After.csv"
 
+#===========================================================Before and After Files===========================================================
         i = 0
+
+        while os.path.exists(output_before_file):
+            i += 1
+            output_before_file = f"New-Before ({i}).csv"
+
+        df_before.to_csv(output_before_file, index=False)
+
+        i = 0
+
+        while os.path.exists(output_after_file):
+            i += 1
+            output_after_file = f"New-After ({i}).csv"
+
+        df_after.to_csv(output_after_file, index=False)
+
+        print(f"\nResults saved in '{output_before_file}' and '{output_after_file}'")
+
+#===========================================================Extra Lines File===========================================================
+        i = 0
+
         while os.path.exists(output_extra_file):
             i += 1
             output_extra_file = f"Extra-Lines-After ({i}).csv"
             
         print(f"\n\n   Generating {output_extra_file}...   \n")
 
-        lines_after.to_csv(output_extra_file, index=False)
+        df_extraLines.to_csv(output_extra_file, index=False)
         
         print(f"\nResult saved in {output_extra_file}\n")
 
     except Exception as e:
-        print(f"Error while analyzing and saving extra lines: {e}")
+        print(f"Error while generating files: {e}")
+
 
 if __name__ == "__main__":
     main()
